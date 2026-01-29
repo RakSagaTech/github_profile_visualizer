9e3c885db0dee899d42794541bfec5325ad87521
rakesh <rakeshsagartfi21@gmail.com>
2026-01-29 09:58:59 +0000

    add GitHub profile fetch, header menu, loader, and failure handling

---
+++ src/components/Header/index.js
@@ -0,0 +1,57 @@
+import './index.css'
+
+const Header = props => {
+  const {
+    menuOptionsList,
+    showMenuOptions,
+    activeMenuId,
+    updateShowMenuOptions,
+  } = props
+
+  const onClickHambergur = () => {
+    updateShowMenuOptions()
+  }
+
+  const renderMenuOptions = () => (
+    <ul className="menu-list">
+      {menuOptionsList.map(eachMenu => {
+        const menuOptionClass =
+          eachMenu.labelId === activeMenuId
+            ? 'active menu-item'
+            : 'not-active menu-item'
+        return (
+          <li key={eachMenu.ladelId} className="menu">
+            <button type="button" className={menuOptionClass}>
+              {eachMenu.menu}
+            </button>
+          </li>
+        )
+      })}
+    </ul>
+  )
+
+  return (
+    <nav className="nav-bar">
+      <div className="title-and-menu">
+        <h1 className="header-title">Github Profile Visualizer </h1>
+        <button
+          type="button"
+          onClick={onClickHambergur}
+          className="hamburger-btn"
+        >
+          <img
+            src="https://res.cloudinary.com/degvq1cfc/image/upload/v1769661552/menu_img_su3fby.png"
+            alt="menu"
+            className="hamburger-img"
+          />
+        </button>
+        <div className="menu-list-container-lg">{renderMenuOptions()}</div>
+      </div>
+      <div className="menu-list-container-sm">
+        {showMenuOptions && renderMenuOptions()}
+      </div>
+    </nav>
+  )
+}
+
+export default Header
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,7 @@
 import {Component} from 'react'
 import {HiOutlineSearch} from 'react-icons/hi'
+import Loader from 'react-loader-spinner'
+import Header from '../Header'
 
 import './index.css'
 
@@ -10,14 +12,108 @@ const apiStatusConstants = {
   failure: 'FAILURE',
 }
 
+const menuOptionsList = [
+  {
+    menu: 'Home',
+    labelId: 'home',
+  },
+  {
+    menu: 'Repositories',
+    labelId: 'repositories',
+  },
+  {
+    menu: 'Analysis',
+    labelId: 'analysis',
+  },
+]
+
 class Home extends Component {
   state = {
-    apiStatus: apiStatusConstants.initial,
+    apiStatus: apiStatusConstants.success,
     username: '',
+    profileDetails: {},
+    showErrMsg: false,
+    showMenuOptions: true,
+    activeMenuId: menuOptionsList[0].labelId,
+  }
+
+  fetchProfileData = async () => {
+    this.setState({
+      apiStatus: apiStatusConstants.inProgress,
+    })
+
+    const {username} = this.state
+    const apiKey = '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'
+    const apiUrl = `https://apis2.ccbp.in/gpv/profile-details/${username}?api_key=${apiKey}`
+    const options = {
+      method: 'GET',
+    }
+
+    try {
+      const response = await fetch(apiUrl, options)
+      if (response.ok) {
+        const data = await response.json()
+        const formattedData = {
+          avatarUrl: data.avatar_url,
+          bio: data.bio,
+          blog: data.blog,
+          company: data.company,
+          createdAt: data.created_at,
+          email: data.email,
+          eventsUrl: data.events_url,
+          followers: data.followers,
+          followersUrl: data.followers_url,
+          following: data.following,
+          followingUrl: data.following_url,
+          gistsUrl: data.gists_url,
+          gravatarId: data.gravatar_id,
+          hireable: data.hireable,
+          htmlUrl: data.html_url,
+          id: data.id,
+          location: data.location,
+          login: data.login,
+          name: data.name,
+          nodeId: data.node_id,
+          organizationsUrl: data.organizations_url,
+          publicGists: data.public_gists,
+          publicRepos: data.public_repos,
+          receivedEventsUrl: data.received_events_url,
+          reposUrl: data.repos_url,
+          siteAdmin: data.site_admin,
+          starredUrl: data.starred_url,
+          subscriptionsUrl: data.subscriptions_url,
+          twitterUsername: data.twitter_username,
+          type: data.type,
+          updatedAt: data.updated_at,
+          url: data.url,
+        }
+        this.setState({
+          profileDetails: formattedData,
+          apiStatus: apiStatusConstants.success,
+          username: '',
+        })
+      } else {
+        this.setState({
+          apiStatus: apiStatusConstants.failure,
+          username: '',
+        })
+      }
+    } catch {
+      this.setState({
+        apiStatus: apiStatusConstants.failure,
+        username: '',
+      })
+    }
+  }
+
+  updateShowMenuOptions = () => {
+    this.setState(prevState => ({
+      showMenuOptions: !prevState.showMenuOptions,
+    }))
   }
 
   onClickSearch = () => {
-    this.fetchUserData()
+    this.fetchProfileData()
   }
 
   onChangeSearch = event => {
@@ -28,7 +124,10 @@ class Home extends Component {
   }
 
   renderSearchInput = () => {
-    const {username} = this.state
+    const {username, showErrMsg} = this.state
+    const searchClassName = showErrMsg
+      ? 'search-input search-failure-input'
+      : 'search-input'
     return (
       <div className="search-container">
         <input
@@ -36,7 +135,7 @@ class Home extends Component {
           placeholder="Enter github username"
           value={username}
           id="searchInput"
-          className="search-input"
+          className={searchClassName}
           onChange={this.onChangeSearch}
         />
         <button
@@ -47,6 +146,44 @@ class Home extends Component {
           onClick={this.onClickSearch}
         >
           <HiOutlineSearch className="search-icon" />
+        </button>
+      </div>
+    )
+  }
+
+  renderProfilePageView = () => {
+    const {profileDetails, showMenuOptions, activeMenuId} = this.state
+    return (
+      <div className="profile-container">
+        <Header
+          menuOptionsList={menuOptionsList}
+          showMenuOptions={showMenuOptions}
+          activeMenuId={activeMenuId}
+          updateShowMenuOptions={this.updateShowMenuOptions}
+        />
+      </div>
+    )
+  }
+
+  renderFailurePageView = () => {
+    const {showErrMsg} = this.state
+    return (
+      <div className="landing-page-container">
+        <div>
+          {this.renderSearchInput()}
+          {showErrMsg && (
+            <p className="err-msg">Enter the valid github username</p>
+          )}
+        </div>
+        <h1 className="failure-title">Github Profile Visualizer</h1>
+        <img
+          src="https://res.cloudinary.com/degvq1cfc/image/upload/v1769657273/failure_img_sg2agc.png"
+          alt="github profile visualizer home page"
+          className="failure-page_img "
+        />
+        <p className="something-went">Something went wrong. Please try again</p>
+        <button type="button" className="try-btn">
+          Try again
         </button>
       </div>
     )
@@ -64,12 +201,24 @@ class Home extends Component {
     </div>
   )
 
+  renderLoadingPageView = () => (
+    <div className="loader-container loader" data-testid="loader">
+      <Loader type="TailSpin" color="#3B82F6" height={50} width={50} />
+    </div>
+  )
+
   renderHomePageView = () => {
     const {apiStatus} = this.state
 
     switch (apiStatus) {
       case apiStatusConstants.initial:
         return this.renderLandingPageView()
+      case apiStatusConstants.failure:
+        return this.renderFailurePageView()
+      case apiStatusConstants.inProgress:
+        return this.renderLoadingPageView()
+      case apiStatusConstants.success:
+        return this.renderProfilePageView()
       default:
         return null
     }
