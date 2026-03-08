42f19e87b0a3165ba7a46df79a5ddf54fd14c623
Sandola Rakesh Sagar <rakeshsagartfi21@gmail.com>
2026-03-08 13:20:54 +0000

    Implement Home component with landing page, search input, loader, and API status handling

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import {Component} from 'react'
+import {Switch, Route} from 'react-router-dom'
+import Home from './components/Home'
+
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    )
+  }
+}
+
+export default App
---
+++ src/components/Home/index.js
@@ -0,0 +1,84 @@
+import {Component} from 'react'
+import {HiOutlineSearch} from 'react-icons/hi'
+import Loader from 'react-loader-spinner'
+
+import './index.css'
+
+const apiStatusConstants = {
+  initial: 'INITIAL',
+  inProgress: 'IN_PROGRESS',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+}
+
+class Home extends Component {
+  state = {
+    apiStatus: apiStatusConstants.initial,
+  }
+
+  renderSearchInput = () => (
+    <div className="search-container">
+      <input
+        type="search"
+        placeholder="Enter github username"
+        id="searchInput"
+        className="search-input"
+      />
+      <button
+        type="button"
+        aria-label="Github Search Icon"
+        data-testid="searchButton"
+        className="search-btn"
+      >
+        <HiOutlineSearch className="search-icon" />
+      </button>
+    </div>
+  )
+
+  renderFailurePageView = () => <p>Failure View</p>
+
+  renderProfilePageView = () => <p>Profile View</p>
+
+  renderLoadingPageView = () => (
+    <div className="loader-container" data-testid="loader">
+      <Loader type="TailSpin" color="#3B82F6" height={50} width={50} />
+    </div>
+  )
+
+  renderLandingPageView = () => {
+    return (
+      <div className="landingpage-container">
+        <div className="search-image-container">
+          {this.renderSearchInput()}
+          <h1 className="home-title">GitHub Profile Visualizer</h1>
+          <img
+            src="https://res.cloudinary.com/degvq1cfc/image/upload/v1769654736/home_img_eaodv3.png"
+            alt="gitHub profile visualizer home page"
+            className="home-image"
+          />
+        </div>
+      </div>
+    )
+  }
+
+  renderHomePageView = () => {
+    const {apiStatus} = this.state
+
+    switch (apiStatus) {
+      case apiStatusConstants.inProgress:
+        return this.renderLoadingPageView()
+      case apiStatusConstants.success:
+        return this.renderProfilePageView()
+      case apiStatusConstants.failure:
+        return this.renderFailurePageView()
+      default:
+        return this.renderLandingPageView()
+    }
+  }
+
+  render() {
+    return <div className="home-container">{this.renderHomePageView()}</div>
+  }
+}
+
+export default Home
